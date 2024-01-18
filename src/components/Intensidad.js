58b942ef7da757374c46d97236fee5ca399fe656
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-01-18 17:07:39 -0600

    LuzIntensidad

--- src/components/Intensidad.js
+++ src/components/Intensidad.js
@@ -3,31 +3,31 @@ import "./styles.intensidad.css";
 
 export function Intensidad() {
   const luz = ["apagado", "encendido al 25%", "encendido al 50%", "encendido al 75%", "encendido al 100%"];
-  const [luzInten, setLuzInten] = useState(0);
+  const [luzIntens, setLuzIntens] = useState(0);
 
   function aumentar() {
-    if (luzInten < luz.length - 1) {
-      setLuzInten(luzInten + 1);
+    if (luzIntens < luz.length - 1) {
+      setLuzIntens(luzIntens + 1);
     } else {
-      setLuzInten(0);
+      setLuzIntens(0);
     }
   }
 
   function disminuir() {
-    if (luzInten > 0) {
-      setLuzInten(luzInten - 1);
+    if (luzIntens > 0) {
+      setLuzIntens(luzIntens - 1);
     } else {
-      setLuzInten(luz.length - 1);
+      setLuzIntens(luz.length - 1);
     }
   }
 
   function resetear() {
-    setLuzInten(0);
+    setLuzIntens(0);
   }
 
   return (
     <div className="intensidad-container">
-      <div className="intensidad-texto">Intensidad de Luz: {luz[luzInten]}</div>
+      <div className="intensidad-texto">Intensidad de Luz: {luz[luzIntens]}</div>
       <button className="boton-aumentar" onClick={aumentar}>Aumentar</button>
       <button className="boton-disminuir" onClick={disminuir}>Disminuir</button>
       <button className="boton-resetear" onClick={resetear}>Resetear</button>
